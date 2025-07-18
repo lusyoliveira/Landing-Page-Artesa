1a95c0fd8e22b0fff55458df124dba3a523e02d0
Luciene <Luciene@DESKTOP-CJTIQ2Q>
2025-07-18 15:24:46 -0300

    Implementado calculo do carrinho de compras

--- js/loja.js
+++ js/loja.js
@@ -1,9 +1,12 @@
-//Slider
 const imagem = document.querySelectorAll('#imagecarrossel img');
 const btnAnterior = document.getElementById('prevBtn');
 const btnProximo = document.getElementById('nextBtn');
 const menuLoja = document.getElementById('menu-loja');
 const btnComprar = document.querySelectorAll('.loja-novidades-comprar');
+const totalProdutos = document.getElementById('total-produtos')
+const desconto = document.getElementById('desconto').textContent
+const frete = document.getElementById('frete').textContent
+const total = document.getElementById('total')
 const carrinhoCompra = JSON.parse(localStorage.getItem('carrinho')) || [];
 
 let currentIndex = 0;
@@ -13,9 +16,13 @@ menuLoja.style.display = 'flex';
 document.addEventListener('DOMContentLoaded', () => {
     if (window.location.pathname.endsWith('carrinho.html')) {
     carregaProdutos(carrinhoCompra)
+
+    totalProdutos.textContent = somarProdutos(carrinhoCompra)
+    total.textContent = calculaPedido() 
 }
 });
 
+//Carrossel
 btnAnterior.addEventListener('click', () => {
     imagem[currentIndex].classList.remove('active');
     currentIndex = (currentIndex === 0) ? imagem.length - 1 : currentIndex - 1;
@@ -81,22 +88,21 @@ function carregaProdutos(listaProdutos) {
      
         const inputQuantidade  = document.createElement('input')
         inputQuantidade.setAttribute ('type', 'text')
-        inputQuantidade.setAttribute ('id', 'quantidade')
         inputQuantidade.setAttribute ('value', produto.quantidade)
 
-        const btnAumentar = document.createElement('button')
+        const btnAumentar = document.createElement('button')   
+        btnAumentar.setAttribute ('title', 'Aumentar Quantidade')
         btnAumentar.onclick = () => aumentaQuantidade(inputQuantidade)
 
         const iconeAumentar = document.createElement('i')
-        iconeAumentar.classList.add('bi', 'bi-dash-lg')
-        iconeAumentar.setAttribute ('id', 'aumentar')
+        iconeAumentar.classList.add('bi','bi-plus-lg')
 
-        const btnDiminuir = document.createElement('button')    
+        const btnDiminuir = document.createElement('button')   
+        btnDiminuir.setAttribute ('title', 'Diminuir Quantidade')   
         btnDiminuir.onclick  = () => diminuiQuantidade(inputQuantidade,produto.descricao)
 
         const iconeDiminuir = document.createElement('i')
-        iconeDiminuir.classList.add('bi', 'bi-plus-lg')
-        iconeDiminuir.setAttribute('id','diminuir')
+        iconeDiminuir.classList.add('bi', 'bi-dash-lg')
   
         const tdValor = document.createElement('td')
         tdValor.textContent = produto.valor
@@ -149,3 +155,26 @@ function diminuiQuantidade(elementoInput, produto) {
 function atualizarQuantidade() {
     localStorage.setItem('quantidade', novaValor)
 };
+
+function somarProdutos(listaProdutos) {
+
+    let soma = 0;
+
+    listaProdutos.forEach(produto => {
+        soma += parseFloat(produto.valor.replace(',', '.')) || 0;
+    });
+
+    return soma.toFixed(2);
+}
+
+function calculaPedido() {
+    let soma = 0
+    let valorProdutos = somarProdutos(carrinhoCompra)
+    let freteConvertido = parseFloat(frete)
+    let descontoConvertido = parseFloat(desconto)
+
+    soma = freteConvertido + parseFloat(valorProdutos) - descontoConvertido
+
+    return soma
+}
+
